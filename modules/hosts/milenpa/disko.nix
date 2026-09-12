93b54cc1256027b3fdd4d73d7c10baf5e6142912
axodentally <24368475+Axolord@users.noreply.github.com>
2026-09-12 22:13:49 +0200

    fix: make disko.nix file a flake-parts module

--- modules/hosts/milenpa/disko.nix
+++ modules/hosts/milenpa/disko.nix
@@ -1,124 +1,125 @@
 { ... }:
-
 let
   disk = "/dev/disk/by-id/nvme-KIOXIA-EXCERIA_PLUS_G3_SSD_5FSKF154Z0EA";
   swapSize = "16G";
 in
 {
-  disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = disk;
+  flake.modules.nixos.milenpa = {
+    disko.devices = {
+      disk.main = {
+        type = "disk";
+        device = disk;
 
-      content = {
-        type = "gpt";
+        content = {
+          type = "gpt";
 
-        partitions = {
-          ESP = {
-            name = "ESP";
-            size = "1G";
-            type = "EF00";
-
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
-            };
-          };
-
-          luks = {
-            name = "luks-cryptroot";
-            size = "100%";
-
-            content = {
-              name = "cryptroot";
-              type = "luks";
-
-              # Do not put a password/key file in the Nix store.
-              # With no keyFile/passwordFile configured, Disko prompts
-              # interactively while it creates the LUKS container.
-              settings = {
-                allowDiscards = true;
-
-                # This is consumed by systemd-cryptsetup in the initrd
-                # after you enroll the TPM token below.
-                crypttabExtraOpts = [
-                  "tpm2-device=auto"
-                ];
-              };
+          partitions = {
+            ESP = {
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
 
               content = {
-                type = "btrfs";
-                extraArgs = [
-                  "-f"
-                  "-L"
-                  "nixos"
-                ];
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
 
-                subvolumes = {
-                  "/@root" = {
-                    mountpoint = "/";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
-                    ];
-                  };
+            luks = {
+              name = "luks-cryptroot";
+              size = "100%";
 
-                  "/@nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
-                    ];
-                  };
+              content = {
+                name = "cryptroot";
+                type = "luks";
 
-                  "/@home" = {
-                    mountpoint = "/home";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
-                    ];
-                  };
+                # Do not put a password/key file in the Nix store.
+                # With no keyFile/passwordFile configured, Disko prompts
+                # interactively while it creates the LUKS container.
+                settings = {
+                  allowDiscards = true;
 
-                  "/@var-lib" = {
-                    mountpoint = "/var/lib";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
-                    ];
-                  };
+                  # This is consumed by systemd-cryptsetup in the initrd
+                  # after you enroll the TPM token below.
+                  crypttabExtraOpts = [
+                    "tpm2-device=auto"
+                  ];
+                };
 
-                  "/@log" = {
-                    mountpoint = "/var/log";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
-                    ];
-                  };
+                content = {
+                  type = "btrfs";
+                  extraArgs = [
+                    "-f"
+                    "-L"
+                    "nixos"
+                  ];
 
-                  "/@srv" = {
-                    mountpoint = "/srv";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
-                    ];
-                  };
+                  subvolumes = {
+                    "/@root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd:3"
+                        "noatime"
+                      ];
+                    };
 
-                  "/@snapshots" = {
-                    mountpoint = "/.snapshots";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
-                    ];
-                  };
+                    "/@nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd:3"
+                        "noatime"
+                      ];
+                    };
 
-                  "/@swap" = {
-                    mountpoint = "/.swapvol";
+                    "/@home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress=zstd:3"
+                        "noatime"
+                      ];
+                    };
 
-                    # Disko creates a Btrfs-appropriate swapfile.
-                    # Do not create an ordinary CoW/compressed file yourself.
-                    swap.swapfile.size = swapSize;
+                    "/@var-lib" = {
+                      mountpoint = "/var/lib";
+                      mountOptions = [
+                        "compress=zstd:3"
+                        "noatime"
+                      ];
+                    };
+
+                    "/@log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = [
+                        "compress=zstd:3"
+                        "noatime"
+                      ];
+                    };
+
+                    "/@srv" = {
+                      mountpoint = "/srv";
+                      mountOptions = [
+                        "compress=zstd:3"
+                        "noatime"
+                      ];
+                    };
+
+                    "/@snapshots" = {
+                      mountpoint = "/.snapshots";
+                      mountOptions = [
+                        "compress=zstd:3"
+                        "noatime"
+                      ];
+                    };
+
+                    "/@swap" = {
+                      mountpoint = "/.swapvol";
+
+                      # Disko creates a Btrfs-appropriate swapfile.
+                      # Do not create an ordinary CoW/compressed file yourself.
+                      swap.swapfile.size = swapSize;
+                    };
                   };
                 };
               };
